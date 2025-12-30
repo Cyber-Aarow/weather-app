e60d8b241a8da2e89a812e6c4d975d25b45acf25
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2025-12-29 19:48:50 -0800

    Create basic HTML layout and initial getWeather function (currently logging in console).

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './main.css';
+
+let location = 'london';
+
+async function getWeather(location){
+    try {
+        const weather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=29GQTWHF9EU6UHLVPZKEFH43C`);
+        const weatherData = await weather.json();
+        console.log(weatherData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getWeather(location);
